5b3127ad8d649eb2ed9a8d6df593d6a807d20c5c
D3boker1 <fmarques_00@protonmail.com>
2023-07-07 01:02:01 +0100

    riscv: comments on vaplic header file

--- src/arch/riscv/irqc/aia/inc/vaplic.h
+++ src/arch/riscv/irqc/aia/inc/vaplic.h
@@ -12,11 +12,6 @@
 #include <bitmap.h>
 #include <emul.h>
 
-/**
- * @brief   Data structure to the virtual interrupt controller when a APLIC 
- *          is the platform interrupt controller.
- * 
- */
 struct vaplic {
     spinlock_t lock;
     size_t idc_num;
@@ -42,7 +37,7 @@ struct vcpu;
  * @brief Initialize the virtual APLIC for a given virtual machine.
  * 
  * @param vm Virtual machine associated to the virtual APLIC
- * @param vaplic_base address base of the physical APLIC
+ * @param arch_vm_platform virtual platform configuration
  * 
  */
 struct arch_vm_platform;
@@ -60,16 +55,23 @@ void virqc_init(struct vm *vm, struct arch_vm_platform arch_vm_platform);
 void vaplic_inject(struct vcpu *vcpu, irqid_t id);
 
 /**
- * @brief For a given virtual machine and a interrupt, associate this interrupt
- *        with the physical one. Thus, interrupt id is mapped to the physical
- *        id source.
+ * @brief For a given virtual machine and an interrupt, associate this 
+ *        interrupt with the physical one. Thus, interrupt id is mapped 
+ *        to the physical id source.
  * 
  * @param vm Virtual machine to associate the 1-1 virt/phys interrupt
  * @param id interrupt identification to associate.
  */
 void vaplic_set_hw(struct vm *vm, irqid_t id);
 
+
 typedef struct vcpu vcpu_t;
+/**
+ * @brief Injects a given interrupt into a virtual cpu
+ * 
+ * @param vcpu target virtual cpu
+ * @param id interrupt id to be injected
+ */
 static inline void virqc_inject(vcpu_t *vcpu, uint64_t id)
 {
     vaplic_inject(vcpu, id);
