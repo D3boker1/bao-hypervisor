cfe02e615d2da6148124bd1c2e137e63e0bf5e26
D3boker1 <fmarques_00@protonmail.com>
2024-05-07 16:12:10 +0100

    feat(platform): add culsans platform

---
+++ src/platform/culsans/platform.mk
@@ -0,0 +1,19 @@
+## SPDX-License-Identifier: GPL-2.0
+## Copyright (c) Bao Project and Contributors. All rights reserved.
+
+# Architecture definition
+ARCH:=riscv
+# CPU definition
+CPU:=
+
+drivers := 8250_uart
+
+platform-cppflags =
+platform-cflags = 
+platform-asflags =
+platform-ldflags =
+
+platform_description:=culsans_desc.c
+
+# Interrupt controller definition
+IRQC:=PLIC
